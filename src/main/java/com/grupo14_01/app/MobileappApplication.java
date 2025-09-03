818f8a80c32a3fc2213096e66503aca21152acc7
Matheus <matheus-mv@outlook.com>
2025-09-03 15:10:13 -0300

    Projeto criado

---
+++ src/main/java/com/grupo14_01/app/MobileappApplication.java
@@ -0,0 +1,13 @@
+package com.grupo14_01.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo14_01/app/MobileappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo14_01.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
