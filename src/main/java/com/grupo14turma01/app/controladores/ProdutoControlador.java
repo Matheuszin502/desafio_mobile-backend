dafb01b196a9bb921ef2cd6b53067f792a37cd81
Matheus <matheus-mv@outlook.com>
2025-09-06 21:02:56 -0300

    CORS habilitado

--- src/main/java/com/grupo14turma01/app/controladores/ProdutoControlador.java
+++ src/main/java/com/grupo14turma01/app/controladores/ProdutoControlador.java
@@ -3,6 +3,7 @@ package com.grupo14turma01.app.controladores;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.grupo14turma01.app.servicos.ProdutoServico;
 
 @RestController
 @RequestMapping("/produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoControlador {
 	
 	@Autowired
